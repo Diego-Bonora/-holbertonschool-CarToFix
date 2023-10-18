25bfa07e905bf9c36345fb89916688c251575a0c
20Emi <iop.emilysanchez1@gmail.com>
2023-10-18 12:32:21 -0700

    update tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,12 +10,29 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'cian-oscuro': '#09B6C2',
-				'cian-claro': '#C5E9EA',
-				'gris-claro': '#CCDBDB',
-				'azul-oscuro': '#026DBB',
-				'color-icons': '#4D88AE',
+        'cian-oscuro': '#09B6C2',
+        'cian-claro': '#C5E9EA',
+        'gris-claro': '#CCDBDB',
+        'azul-oscuro': '#026DBB',
+        'color-icons': '#4D88AE',
 			},
+			height: {
+        'bloque': '95%',
+        'bloque_login': '85%',
+      },
+      fontFamily: {
+        'Inter': ['Inter', 'sans'],
+			},
+			spacing: {
+        'pad-1': '7%',
+        'pad-2': '25%',
+        'pad-3': '3%',
+        'pad-4': '30%',
+        'marg-1': '8%',
+        'marg-2': '15%',
+        'marg-3': '5%',
+        'marg-4': '10%',
+      },
 		},
 	},
 	plugins: [],
