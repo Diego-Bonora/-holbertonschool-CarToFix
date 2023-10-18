e93ba23ed22bf98daddb9b6eeb566510e3437e56
KrasniKot <emanueltrias9@gmail.com>
2023-10-18 10:17:21 -0700

    new tests added, previous commit

---
+++ models/type_vehicle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+""" Contains class Type Vehicle """
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Type_vehicle(BaseModel, Base):
+    """ Representation of Type Vehicle """
+    __tablename__ = 'type_vehicles'
+    name = Column(String(30), nullable=False)
+    vehicles = relationship("Vehicle",
+                            backref="type_vehicles",
+                            cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """ initializes Type Vehicle """
+        super().__init__(*args, **kwargs)
