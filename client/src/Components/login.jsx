08abb525cefd4310a6e5816a5b351f92f6ea1619
SantiagoC16 <6317@holbertonstudents.com>
2023-10-23 16:17:25 -0700

    login

--- client/src/Components/login.jsx
+++ client/src/Components/login.jsx
@@ -1,14 +1,61 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Envelope } from 'react-bootstrap-icons';
 import InputFied from './InputFied';
 import PasswordInput from './PasswordInput';
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
 
 export default function Login() {
   const [recuerdame, setRecuerdame] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const loginRequest = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/v1/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          recuerdame: recuerdame,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        if (recuerdame) {
+          Cookies.set('cookie_user', '', { expires: 7 });
+        }
+        navigate('/home');
+      } else if (response.status === 400) {
+        setError(data.error);
+      } else if (response.status === 404) {
+        setError(data.error);
+      } else if (response.status === 401) {
+        setError(data.error);
+      } else {
+        setError('An error occurred. Please try again later.');
+      }
+    } catch (error) {
+      setError('An error occurred. Please try again later.');
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (email && password) {
+      loginRequest();
+    }
+  }, [email, password, recuerdame]);
+
   return (
     <>
     <div className='2xl:w-2/5 h-screen'>
@@ -18,17 +65,17 @@ export default function Login() {
             <div className="rounded-full border-2 border-white mx-auto w-48 h-48 bg-gris-claro translate-y-1/2"></div>
             <div className="text-white border-4 my-auto rounded-2xl bg-cian-oscuro 2xl:h-4/5 border-gris-claro">
               <form action="" className="items-center justify-center px-pad-1 pb-pad-1 pt-pad-2 font-Inter">
-                <InputFied icon={<Envelope />} type='email' placeholder='Email' value={email} onChange={(emailEvent) => setEmail(emailEvent.target.value)}/>
-                <PasswordInput PasswordInput='Contraseña' value={password} onChange={(passwordEvent) => setPassword(passwordEvent.target.value)}/>
+                <InputFied icon={<Envelope />} type='email' placeholder='Email' value={email} onChange={(emailEvent) => setEmail(emailEvent.target.value)} />
+                <PasswordInput PasswordInput='Contraseña' value={password} onChange={(passwordEvent) => setPassword(passwordEvent.target.value)} />
                 <div className="flex justify-between items-center mt-marg-3">
                   <div className="flex gap-1 items-center">
-                    <input type="checkbox" className="w-4 h-4 border-2 border-white rounded-full" onChange={(remembermeEvent) => setRecuerdame(remembermeEvent.target.checked)}/>
+                    <input type="checkbox" className="w-4 h-4 border-2 border-white rounded-full" onChange={(remembermeEvent) => setRecuerdame(remembermeEvent.target.checked)} />
                     <label htmlFor="Recuerdame" className="2xl:text-base underline">Recuerdame</label>
                   </div>
                   <Link to='' className="2xl:text-base text-white underline">Olvide mi contraseña</Link>
                 </div>
                 <div className="mx-marg-3 ">
-                  <button type="submit"className="text-center font-bold font-Inter 2xl:text-xl text-xl p-pad-3 bg-azul-oscuro rounded-full mb-marg-1 mt-marg-2 w-full mt-12">Iniciar sesión</button>
+                  <button type="submit" className="text-center font-bold font-Inter 2xl:text-xl text-xl p-pad-3 bg-azul-oscuro rounded-full mb-marg-1 mt-marg-2 w-full mt-12">Iniciar sesión</button>
                 </div>
                 <div className='px-12'>
                   <span className="2xl:text-lg text-base mt-4 font-bold">¿Eres nuevo? <Link to='/Signin' className='text-azul-oscuro underline' > Registrate</Link></span>
