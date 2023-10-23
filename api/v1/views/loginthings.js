9657f322a3c266c7072074da46411a5e466c3bf9
SantiagoC16 <6317@holbertonstudents.com>
2023-10-22 20:46:38 -0700

    login js prototype updated

--- api/v1/views/loginthings.js
+++ api/v1/views/loginthings.js
@@ -1,10 +1,17 @@
 $.ajax({
-  url: '/login',
+  url: 'http://localhost:5000/api/v1/login',
   type: 'POST',
   contentType: 'application/json',
-  data: JSON.stringify(/* login data */),
+  data: JSON.stringify({
+    email: email,
+    password: password,
+    recuerdame: recuerdame
+  }),
   success: function (data) {
     if (data.message === 'Logged in successfully') {
+      if (recuerdame) {
+        Cookies.set('cookie_user', '', { expires: 7 });
+      }
       // window.location.href = '/home';
     }
   }
