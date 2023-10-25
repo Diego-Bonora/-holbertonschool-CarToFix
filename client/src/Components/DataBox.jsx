b20f3c5713b9ff756b026f415ab1e4d7b21b4245
20Emi <iop.emilysanchez1@gmail.com>
2023-10-25 13:15:08 -0700

    databox incomplete

--- client/src/Components/DataBox.jsx
+++ client/src/Components/DataBox.jsx
@@ -4,36 +4,43 @@
 import React from 'react';
 import Button from './button';
 
+
 export default function DataBox({info, columns }) {
 
 	return (
-  <div className=''>
+  <div className='w-full'>
     <table className='text-black my-5 w-full'>
-      <div className='header-div bg-gris-footer m-1 '>
+      <div className='header-div bg-gris-footer rounded-lg m-0.5 my-2.5 w-list'>
       <thead>
-        <tr>
+        <tr className='w-list'>
           {columns.map((columns, index) => (
-            <th key={index} className='px-3 ' >{columns} </th>
+            <th key={index} className='p-3  align-middle border-l flex-row justify-around w-1/2' >
+              <div className='w-full'>{columns}</div>
+              </th>
           ))}
         </tr>
       </thead>
       </div>
-      <tbody className='w-9/12'>
+      <tbody>
         {info.map((i, index) => {
           return (
-            <div key={index} className="row-div bg-gris-footer rounded-lg m-1">
-              <tr className="text-black">
+            
+              <tr key={index} className="text-black flex flex-grow items-center">
                 {/* Recorre cada nombre de columna uno por uno" */}
+                <div  className="row-div bg-gris-footer rounded-lg m-0.5 w-list">
                 {columns.map((column, indexcol) => (
-                  <td key={indexcol} className="px-5">
+                  <td key={indexcol} className="p-2 px-8 align-middle border-l">
                     {i[column]} {/* Muestra el valor que debe ir según el nombre de la columna */}
                   </td>
                 ))}
-                <td className="px-5 py-15">
+                </div>
+                <div className='flex justify-end gap-4 w-10 ml-10'>
+                <td>
                   <Button children="ver" color="orange" size="mini" />
                 </td>
+                </div>
               </tr>
-            </div>
+            
           );
         })}
       </tbody>
