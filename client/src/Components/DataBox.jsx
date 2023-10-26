f06db82aa7c682b602dceb2a575b460454f185e7
20Emi <iop.emilysanchez1@gmail.com>
2023-10-26 06:19:36 -0700

    update

--- client/src/Components/DataBox.jsx
+++ client/src/Components/DataBox.jsx
@@ -4,18 +4,17 @@
 import React from 'react';
 import Button from './button';
 
-
 export default function DataBox({info, columns }) {
-
+  
 	return (
   <div className='w-full'>
     <table className='text-black my-5 w-full'>
-      <div className='header-div bg-gris-footer rounded-lg m-0.5 my-2.5 w-list'>
+      <div className='header-div bg-gris-footer rounded-lg m-0.5 my-2.5 sm:w-list w-list-1'>
       <thead>
-        <tr className='w-list'>
+        <tr className='w-screen'>
           {columns.map((columns, index) => (
-            <th key={index} className='p-3  align-middle border-l flex-row justify-around w-1/2' >
-              <div className='w-full'>{columns}</div>
+            <th key={index} className='p-3 sm:px-11 px-8 align-middle border-l flex-row justify-around text-lg' >
+              {columns}
               </th>
           ))}
         </tr>
@@ -27,16 +26,16 @@ export default function DataBox({info, columns }) {
             
               <tr key={index} className="text-black flex flex-grow items-center">
                 {/* Recorre cada nombre de columna uno por uno" */}
-                <div  className="row-div bg-gris-footer rounded-lg m-0.5 w-list">
+                <div  className="row-div bg-gris-footer rounded-lg m-0.5 sm:w-list w-list-1">
                 {columns.map((column, indexcol) => (
-                  <td key={indexcol} className="p-2 px-8 align-middle border-l">
+                  <td key={indexcol} className="p-2 sm:px-8 px-5 align-middle border-l text-md">
                     {i[column]} {/* Muestra el valor que debe ir según el nombre de la columna */}
                   </td>
                 ))}
                 </div>
-                <div className='flex justify-end gap-4 w-10 ml-10'>
+                <div className='flex justify-end gap-4 w-10 lg:ml-9 md:ml-7 ml-5'>
                 <td>
-                  <Button children="ver" color="orange" size="mini" />
+                  <button type='' className='bg-orange-claro hover:bg-orange-oscuro text-white p-px md:w-14 w-10 items-center'>Ver</button>
                 </td>
                 </div>
               </tr>
--- client/src/Components/Vehicle_history.jsx
+++ client/src/Components/Vehicle_history.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
 import DataBox from './DataBox';
+import NewBudgetButton from './NewBudgetButton';
 
 
 export default function Vehicle_history() {
@@ -9,19 +10,19 @@ export default function Vehicle_history() {
   let info_vehiculo = [{ Marca: 'Hyundai', Modelo: 'Electra', Color: 'Gris'
   }]
   const columns = ['Ingreso', 'detalle'];
-  const data = [{ Ingreso: '24/10/2022', detalle: 'Cambio de aceite'},
+  const data = [{ Ingreso: '24/10/2022', detalle: 'Cambio de aceite hjsen jhndxecWUO HNDXEJKWHCI'},
   { Ingreso: '24/10/2022', detalle: 'Chequeo general dl condnsador de flujos'},
   { Ingreso: '24/10/2022', detalle: 'Revision de frenos'},
 	{ Ingreso: '24/10/2022', detalle: 'Electricidad'},
 	{ Ingreso: '24/10/2022', detalle: 'Tren delantero'},
   { Ingreso: '24/10/2022', detalle: 'Tren delantero'},
-  { Ingreso: '24/10/2022', detalle: 'Tren delantero'}];
+  { Ingreso: '24/10/2022', detalle: ''}];
     return (
 		<>
         <div className='w-screen h-screen bg-page_background'>
             <NavBar />
             {/* info del vehiculo y matricula*/}
-            <div className='bg-gris-background mr-marg-5 ml-marg-4 mt-marg-3 flex flex-wrap h-info_vehiculo rounded-r-lg'>
+            <div className='bg-gris-background lg:mr-marg-5 mr-marg-1 lg:ml-marg-4 ml-marg-1 mt-marg-3 flex flex-wrap h-info_vehiculo rounded-r-lg'>
               {/* matricula general */}
               <div className='border border-azul-oscuro flex flex-col justify-start w-3/12 h-full' >
                 {/*tipo de vehiculo*/}
@@ -36,7 +37,7 @@ export default function Vehicle_history() {
               </div>
               <div className='pl-10 w-9/12 flex  h-full'>
                 {info_vehiculo.map((info, index) => (
-                <div key={index} className='text-black flex flex-col flex-wrap text-xl my-4 w-9/12'>
+                <div key={index} className='text-black sm:flex sm:flex-col sm:flex-wrap hidden text-xl my-4 w-9/12'>
                   <div  className='py-2'>
                     Marca: <span className='font-bold'>{info.Marca}</span></div>
                   <div className='pb-1'>
@@ -48,11 +49,11 @@ export default function Vehicle_history() {
               </div>
             </div>
             {/* info del historial */}
-            <div className='bg-gris-background mr-marg-5 ml-marg-4 mt-marg-3 flex flex-wrap rounded-lg'>
-                <div className="data w-full flex flex-col items-center">
-        
+            <div className='bg-gris-background items-center lg:mr-marg-5 mr-marg-1 lg:ml-marg-4 ml-marg-1 mt-marg-3 flex flex-wrap rounded-lg justify-items-center justify-center'>
+                <div className="data w-full flex flex-col ml-10">
                   <DataBox columns={columns} info={data}/>
                 </div>
+            <NewBudgetButton />
             </div>
         </div>
 		</>
