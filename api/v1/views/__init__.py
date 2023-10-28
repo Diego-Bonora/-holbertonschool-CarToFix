60b6c88fce43982fefe50ca6b317e31045e95d40
SantiagoC16 <6317@holbertonstudents.com>
2023-10-28 14:49:54 -0700

    added register to imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.user import *  # noqa:E402
 from api.v1.views.login import *  # noqa:E402
+from api.v1.views.register import *  # noqa:E402
