0d867b4f2122cd25392ef55cd1f9ae0d7b4be5cc
20Emi <iop.emilysanchez1@gmail.com>
2023-10-28 20:53:07 -0700

    completed

--- client/src/Components/DataBox.jsx
+++ client/src/Components/DataBox.jsx
@@ -4,44 +4,46 @@ import Button from './button';
 
 export default function DataBox({ info, columns }) {
   return (
-    <table className="text-black my-5 w-full ">
+
+    <table className="text-black my-5 w-full">
       <thead>
-        <tr className="w-screen bg-gris-footer border-b-8 border-t-10 border-r-1">
+        <tr className="w-screen bg-gris-footer">
           {columns.map((column, index) => (
             <th
               key={index}
-              className={`p-3 align-middle border-l flex-row justify-around text-lg
-                ${index === 0 ? 'rounded-tl-2xl rounded-bl-2xl' : index === columns.length - 1 ? 'rounded-tr-2xl rounded-br-2xl' : ''}`}
+              className={`p-3 align-middle flex-row justify-around md:text-lg text-base
+                ${index === 0 ? 'rounded-tl-2xl rounded-bl-2xl border-r-2' : index === columns.length - 1 ? 'rounded-tr-2xl rounded-br-2xl border-l-2' : ''}`}
             >
               {column}
             </th>
           ))}
-        </tr>
+        </tr><div className='mb-3'></div>
       </thead>
       <tbody>
         {info.map((data, index) => (
-          <tr
+          <><tr
             key={index}
-            className=" border-4 border-r-1"
+            className=""
           >
             {columns.map((column, indexcol) => (
               <td
                 key={indexcol}
-                className={`p-2 sm:px-8 px-5 align-middle border-l text-md 
-                ${indexcol === 0 ? 'rounded-tl-2xl rounded-bl-2xl bg-gris-footer' : indexcol === columns.length - 1 ? 'rounded-tr-2xl rounded-br-2xl bg-gris-footer' : ''} 
+                className={`p-2 sm:px-8 px-5 align-middle  md:text-base text-sm
+                ${indexcol === 0 ? 'rounded-tl-2xl rounded-bl-2xl bg-gris-footer border-r-2' : indexcol === columns.length - 1 ? 'rounded-tr-2xl rounded-br-2xl bg-gris-footer border-l-2' : ''} 
                 ${indexcol === Math.floor(columns.length / 2) ? 'bg-gris-footer' : ''}`}
               >
                 {data[column]}
               </td>
             ))}
-            <td className="p-2 sm:px-8 px-5 align-middle border-l text-md bg-opacity-0">
+            <td className="p-2 sm:px-8 px-5 align-middle md:text-md text-sm">
               <button type="button" className="bg-orange-claro hover:bg-orange-oscuro text-white p-px md:w-14 w-10 items-center">
                 Ver
               </button>
             </td>
-          </tr>
+          </tr><div className='mb-1'></div></>
         ))}
       </tbody>
     </table>
+
   );
 }
