47c0f7e708ba871d086855f3ed9db0d95f69df11
20Emi <iop.emilysanchez1@gmail.com>
2023-10-28 20:54:16 -0700

    small details remain

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div>
-      <header>
-        <div className="py-3 pr-14 pl-8 bg-cian-oscuro h-[80px] rounded-b-header_border flex items-center">
-          <div className='flex flex-grow items-center'>
-            <div className='w-16 h-16 bg-slate-400 rounded-full border-2'></div>
-          </div>
-          <nav className="flex justify-end gap-4 self-center w-2/5">
-            <Link to='' className='w-40 bg-azul-oscuro inline-block rounded text-center py-2 text-white text-sm font-medium shadow-[0_4px_9px_-4px_#3b71ca] hover:bg-shadow_header hover:text-white'>Vehículos</Link>
-            <Link to='' className='w-40 bg-azul-oscuro inline-block rounded text-center text-white py-2 text-sm font-medium shadow-[0_4px_9px_-4px_#3b71ca] hover:bg-shadow_header hover:text-white'>Presupuestos</Link>
-            <Link to='' className='w-40 bg-azul-oscuro inline-block rounded text-center text-white py-2 text-sm font-medium shadow-[0_4px_9px_-4px_#3b71ca] hover:bg-shadow_header hover:text-white'>Servicios</Link>
-          </nav>
+    <header>
+      <div className=" py-3 pr-14 pl-8 bg-cian-oscuro md:rounded-b-header_border flex items-center justify-between">
+        <div className='flex flex-grow items-center'>
+          <div className='w-16 h-16 bg-slate-400 rounded-full border-2'></div>
         </div>
-      </header>
-    </div>
+        {/* Icono de menú */}
+        <div className="md:hidden pr-10">
+        {menuOpen ? (
+            <FaTimes onClick={toggleMenu} className='w-7 h-7'/>
+          ) : (
+            <FaBars onClick={toggleMenu} className='w-7 h-7'/>
+          )}
+        </div>
+        <nav className={` md:flex md:justify-end md:gap-4 md:w-2/5 md:static top-[80px] fixed bg-cian-oscuro/95 w-full h-full flex flex-col items-center justify-center left-0 md:flex-row text-2xl rounded-bl-full transition-transform duration-200 ease-in-out
+        ${menuOpen ? 'block' : 'hidden'}`}>
+          <Link to='' className='w-40 md:bg-azul-oscuro inline-block rounded text-center py-2 text-white md:text-sm md:font-medium md:shadow-[0_4px_9px_-4px_#3b71ca] md:hover:bg-shadow_header md:hover:text-white'>Vehículos</Link>
+          <Link to='' className='w-40 md:bg-azul-oscuro inline-block rounded text-center text-white py-2 md:text-sm font-medium md:shadow-[0_4px_9px_-4px_#3b71ca] md:hover:bg-shadow_header md:hover:text-white'>Presupuestos</Link>
+          <Link to='' className='w-40 md:bg-azul-oscuro inline-block rounded text-center text-white py-2 md:text-sm font-medium md:shadow-[0_4px_9px_-4px_#3b71ca] md:hover:bg-shadow_header md:hover:text-white'>Servicios</Link>
+        </nav>        
+          </div>
+    </header>
   )
 }
