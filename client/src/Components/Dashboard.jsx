8a002123e4fa048995716249ef16d9c5a34b79bf
20Emi <iop.emilysanchez1@gmail.com>
2023-10-29 17:53:10 -0700

    update vehicle_history

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import Confirmation from './Confirmations'
-import DataBox from './DataBox'
-import DataFrame from './DataFrame'
-import NewBudgetButton from './NewBudgetButton'
-import TitleBox from './TitleBox'
+
+import NavBar from './NavBar'
 
 export default function Dashboard() {
 
@@ -32,38 +29,8 @@ export default function Dashboard() {
 
 	return (
 		<>
-			<div className=' bg-[#F5F5F5] h-sreen'>
-				<div className='father flex flex-wrap  max-w-full mt-5'>
-					<div className='flex flex-wrap md:grid md:grid-cols-1 p-4 md:place-content-evenly justify-center'>
-						<div className="flex flex-wrap h-fit md:grid md:grid-cols-2 md:w-screen justify-items-center justify-center">
-							{/* Titulo y DataFrames */}
-							<div className='h-20 md:mb-10 mb-20 md:w-full md:h-full justify-center -translate-y-16'>
-								<TitleBox title="Nombre de Taller" />
-							</div>
-							<div className="flex flex-wrap h-full md:grid md:grid-cols-2 md:gap-8 md:place-items-center justify-items-center justify-center min-w-[50px] space-x-5 " >
-
-								<DataFrame title="Presupuestos en espera" level="12" />
-
-								<DataFrame title="Total de Vehiculos" level="6" />
-
-							</div>
-						</div>
-						{/* Databoxes inferiores con detalles */}
-						<div className="dataframe flex flex-wrap w-screen md:grid md:grid-cols-2 justify-items-center justify-center mr-10">
-							<DataBox title="Servicios Activos" info={data} />
-							<div className='flex flex-col-1 flex-wrap justify-end'>
-								<Confirmation title="Confirmaciones Recientes" info={data} />
-								{/* BOTON NUEVO PRESUPUESTO */}
-
-								<NewBudgetButton className="flex justify-end" />
-
-							</div>
-
-						</div>
-					</div>
-
-				</div>
-			</div>
+		<NavBar />
 		</>
+
 	)
 }
--- client/src/Components/NewBudgetButton.jsx
+++ client/src/Components/NewBudgetButton.jsx
@@ -4,13 +4,13 @@ import wheel from '../assets/wheel.webp';
 
 export default function NewBudgetButton() {
   return (
-    <div className='absolute bottom-0 right-0 py-5 pr-28 pl-5'>
+    <div className='absolute bottom-0 right-0 lg:py-5 pr-28 pl-5'>
       <Link to="/NewBudget">
-        <div className='button w-24 h-24 flex flex-col justify-end items-center mt-5'>
+        <div className='button lg:w-24 lg:h-24 w-16 h-16 flex flex-col justify-end items-center mt-5'>
           <div className="image w-30 h-30">
             <img src={wheel} alt="Wheel" />
           </div>
-          <span className='font-black text-center mt-2 text-2xl'>Nuevo Presupuesto</span>
+          <span className='font-black text-center mt-2 lg:text-2xl text-lg'>Nuevo Presupuesto</span>
         </div>
       </Link>
     </div>
