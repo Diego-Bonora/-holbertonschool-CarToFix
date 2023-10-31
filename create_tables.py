d20937358a66509b6250aa68c62efad0ff75184d
Diego-Bonora <bonoradi@gmail.com>
2023-10-31 06:47:29 -0700

    file that creates all the tables for the database

---
+++ create_tables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" file that creates all the tables """
+
+from models.user import User
+from models.client import Client
+from models.service import Service
+from models.vehicle import Vehicle
+from models.budget import Budget
+from models.type_vehicle import TypeVehicle
+from models.brand import Brand
+from models.workers import Worker
