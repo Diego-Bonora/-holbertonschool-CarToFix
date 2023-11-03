4e29e8b064b3b5c53605dab191d7e7a369ea7758
20Emi <iop.emilysanchez1@gmail.com>
2023-11-03 13:07:34 -0700

    update

--- client/src/Components/Vehicle.jsx
+++ client/src/Components/Vehicle.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar';
 import DataBox from './DataBox';
 import ButtonService from './ButtonService';
 import Searchbar from './Searchbar';
 import {FaTh} from 'react-icons/fa'
+import axios from 'axios';
 
 
 export default function Vehicle_history() {
 
-  const columns = ['Matricula', 'Servicio', 'Ultima'];
-  const data = [{ Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},
-  { Matricula: 'ABC 4545', Servicio: 'Electricidad', Ultima: '03/12/2022'},];
+    {/* column for databox*/}
+
+    const vehiclecolumn = ['Matrícula', 'Servicio', 'Creación'];
+
+    const baseURL = 'http://127.0.0.1:5000'
+
+    const [VehicleData, setVehicleData] = useState([
+      {
+        Matrícula: '',
+        Servicio: '',
+        Creación: '',
+      }
+    ]);
+
+    {/*get plate*/}
+    useEffect(() => {
+      axios.get(`${baseURL}/api/v1/vehicle`)
+        .then(res => {
+          const VehicleData = res.data;
+          const data = VehicleData.map(vehicle => ({
+            Matrícula: Object.values(vehicle)[0]
+          }));
+          setVehicleData(data);
+        })
+        .catch(error => {
+          console.error('Error', error);
+        });
+    }
+    )
     return (
 		<>
         <div className='w-screen h-screen bg-page_background'>
@@ -32,7 +52,7 @@ export default function Vehicle_history() {
             {/* info del historial */}
             <div className='bg-tabla_service items-center lg:h-info_history h-info_history_2 lg:mr-marg-5 mr-marg-1 lg:ml-marg-4 ml-marg-1 mt-marg-3 flex flex-wrap rounded-lg justify-items-center justify-center shadow-md shadow-gray-300 h-30'>
             <div className='overflow-y-scroll h-full w-full ml-9'>
-                <DataBox columns={columns} info={data}/>
+                <DataBox columns={vehiclecolumn} info={VehicleData}/>
               </div>
             <ButtonService />
             </div>
