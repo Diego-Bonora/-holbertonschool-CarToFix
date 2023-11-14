0e2304670936f9e7ba478a207ba3a6b109fbfe80
20Emi <iop.emilysanchez1@gmail.com>
2023-11-13 17:57:28 -0800

    updated

---
+++ client/src/Pages/DataBox.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export default function DataBox({ info, columns, SeeClick, IdName, onRedirect, columnsName}) {
+  return (
+
+    <table className="text-black mb-5 mt-14 w-full ">
+      <thead>
+        <tr className="w-screen bg-gris-footer">
+          {columns.map((column, index) => (
+            <th
+              key={index}
+              className={`p-3 align-middle flex-row justify-around md:text-lg text-base
+                ${index === 0 ? 'rounded-tl-2xl rounded-bl-2xl border-r-2' : index === columns.length - 1 ? 'rounded-tr-2xl rounded-br-2xl border-l-2' : ''}`}
+            >
+              {columnsName[column]}
+            </th>
+          ))}
+        </tr><div className='mb-3'></div>
+      </thead>
+      <tbody>
+        {info.map((data, index) => (
+          <><tr
+            key={index}
+            className=""
+          >
+            {columns.map((column, indexcol) => (
+              <td
+                key={indexcol}
+                className={`p-2 px-5 align-middle  md:text-base text-sm text-center
+                ${indexcol === 0 ? 'rounded-tl-2xl rounded-bl-2xl bg-gris-footer border-r-2' : indexcol === columns.length - 1 ? 'rounded-tr-2xl rounded-br-2xl bg-gris-footer border-l-2' : ''} 
+                ${indexcol === Math.floor(columns.length / 2) ? 'bg-gris-footer' : ''}`}
+              >
+                {data[column]}
+              </td>
+            ))}
+            <td className="p-2 sm:px-8 pl-5 align-middle md:text-md text-sm">
+              <button type="button" className="bg-orange-claro hover:bg-orange-oscuro text-white p-px md:w-14 w-10 items-center"
+              onClick={() => SeeClick(data[IdName], onRedirect)}>
+                Ver
+              </button>
+            </td>
+          </tr><div className='mb-1'></div></>
+        ))}
+      </tbody>
+    </table>
+
+  );
+}
---
+++ client/src/Pages/Searchbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { AiOutlineSearch } from 'react-icons/ai';
+
+export default function Search({ onSearch }) {
+    const [Query, setQuery] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault(); {/*evita la recarga de la pagina cuando se utiliza el buscador */}
+        onSearch(Query);
+    }
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+        onSearch(value || '');
+    }
+  return (
+    <form className='w-2/5 relative'>
+      <div className="relative">
+        <input
+          type="search"
+          placeholder='Buscar matricula'
+          value={Query}
+          onChange={handleChange}
+          className='w-full p-1 pl-4 rounded-2xl bg-gris-footer'
+        />
+        <button onClick={handleSearch} className='absolute right-1 top-1/2 -translate-y-1/2 p-1 bg-gris-footer rounded-full'>
+          <AiOutlineSearch />
+        </button>
+      </div>
+    </form>
+  );
+}
